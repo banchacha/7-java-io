b3ff507330222e51d50e824c2d13db5fdd17de91
Anna <ann2005ann55@gmail.com>
2025-03-14 21:30:41 +0500

    Коноплёва

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,6 +1,7 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Task02Main {
     public static void main(String[] args) throws IOException {
@@ -9,5 +10,12 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        int prev = System.in.read(), next;
+        while (prev != -1) {
+            next = System.in.read();
+            if (prev != 13 || next != 10) System.out.write(prev);
+            prev = next;
+        }
+        System.out.flush();
     }
 }
